3fd5c1b524a07fb20b2a85e5f5a520dcd71822b0
Mike Cole <MikeColeGuru@gmail.com>
2021-12-17 00:57:42 -0600

    Add GPL 2 license

--- src/cdb_module.ts
+++ src/cdb_module.ts
@@ -1,3 +1,21 @@
+/*
+    CDB Debug File - Modules
+    Copyright (C) 2021 Mike Cole <MikeColeGuru@gmail.com>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; version 2 of the License
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA
+*/
+
 import { CDB, EXEPoint } from "./cdb"
 import { resolve, basename } from "path"
 import { readdir } from "fs/promises"
